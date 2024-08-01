011622ae192ed65d499452588214926f0027d6c9
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-08-01 21:08:33 +0530

    song & image upload to cloudinary

--- spotify-backend/src/controllers/songController.js
+++ spotify-backend/src/controllers/songController.js
@@ -1,5 +1,21 @@
-const addSong = async (req,res) => {
+import {v2 as cloudinary} from 'cloudinary';
+import songModel from '../models/songModel.js';
 
+const addSong = async (req,res) => {
+try {
+    const name = req.body.name;
+    const desc = req.body.desc;
+    const album = req.body.album;
+    const audioFile = req.files.audio[0];
+    const imageFile = req.files.image[0];
+    const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type:"video"});
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image"})
+
+    console.log(name,desc,album,audioUpload,imageUpload);
+
+} catch (error) {
+    
+}
 }
 
 const listSong = async(req,res) => {
