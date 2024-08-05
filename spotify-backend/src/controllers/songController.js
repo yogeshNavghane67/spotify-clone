69fceb34413935fbc76c42ee7f3c51d8e1f2eba0
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-08-05 23:11:10 +0530

    add song data to db

--- spotify-backend/src/controllers/songController.js
+++ spotify-backend/src/controllers/songController.js
@@ -1,37 +1,41 @@
-import {v2 as cloudinary} from 'cloudinary';
-import songModel from '../models/songModel.js';
+import { v2 as cloudinary } from "cloudinary";
+import songModel from "../models/songModel.js";
 
-const addSong = async (req,res) => {
-try {
+const addSong = async (req, res) => {
+  try {
     const name = req.body.name;
     const desc = req.body.desc;
     const album = req.body.album;
     const audioFile = req.files.audio[0];
     const imageFile = req.files.image[0];
-    const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type:"video"});
-    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image"})
+    const audioUpload = await cloudinary.uploader.upload(audioFile.path, {
+      resource_type: "video",
+    });
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const duration = `${Math.floor(audioUpload.duration / 60)}:${Math.floor(
+      audioUpload.duration % 60
+    )}`;
 
-    console.log(name,desc,album,audioUpload,imageUpload);
+    const songData = {
+      name,
+      desc,
+      album,
+      image: imageUpload.secure_url,
+      file: audioUpload.secure_url,
+      duration,
+    };
 
-} catch (error) {
-    
-}
-}
+    const song = songModel(songData);
+    await song.save();
 
-const listSong = async(req,res) => {
+    res.json({ success: true, message: "Song Added" });
+  } catch (error) {
+    res.json({ success: false });
+  }
+};
 
-}
+const listSong = async (req, res) => {};
 
-export {addSong,listSong}
-
-
-
-
-
-
-
-
-
-
-
-
+export { addSong, listSong };
